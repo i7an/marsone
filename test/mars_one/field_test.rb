6ee9357195ba7d3b82c2af375a395a2807631986
Ivan Yurchanka <vanya.yu@gmail.com>
2017-05-09 23:12:34 +0300

    field class with tests

---
+++ test/mars_one/field_test.rb
@@ -0,0 +1,27 @@
+require 'minitest/autorun'
+require 'mars_one/field'
+
+class FieldTest < Minitest::Test
+  Point = Struct.new('Point', :x, :y)
+
+  def test_validation
+    field = MarsOne::Field.new(5, 5)
+    assert_equal(true, field.valid?(Point.new(0, 0)))
+    assert_equal(true, field.valid?(Point.new(5, 5)))
+    assert_equal(false, field.valid?(Point.new(-1, 0)))
+    assert_equal(false, field.valid?(Point.new(6, 0)))
+  end
+
+  def test_validate
+    field = MarsOne::Field.new(5, 5)
+    assert_raises(MarsOne::Field::InvalidLocationError) do
+      field.validate!(Point.new(6, 6))
+    end
+  end
+
+  def test_forbid
+    field = MarsOne::Field.new(5, 5)
+    field.forbid(Point.new(3, 3))
+    assert_equal(false, field.valid?(Point.new(3, 3)))
+  end
+end
