6afafb89e5d378ee41a8ff16b0f89a8c132305c1
Ivan Yurchanka <vanya.yu@gmail.com>
2017-05-14 00:10:22 +0300

    refactor mission class

--- lib/mars_one/mission.rb
+++ lib/mars_one/mission.rb
@@ -12,33 +12,43 @@ module MarsOne
     end
 
     def execute
-      rovers = []
+      field = build_field(mission_reader.read_field)
 
-      field_max_x, field_max_y = mission_reader.read_field
-      field = MarsOne::Models::Field.new(field_max_x, field_max_y)
-
-      mission_reader.each_rover do |(x, y, dir), commands|
-        direction = MarsOne::MissionParser::Utils.direction_to_point(dir)
-        position = MarsOne::Models::Point.new(x, y)
-        rover = MarsOne::Models::Rover.new(position, direction)
+      mission_reader.each_rover.map do |(x, y, direction), commands|
+        rover = build_rover(x, y, direction)
+        
         field.validate!(rover.position)
 
-        commands.each_char do |command_letter|
-          command = MarsOne::MissionParser::CommandFactory.command(command_letter)
-          command.execute(rover)
+        commands.each_char do |command_char|
+          apply_command(rover, command_char)
           field.validate!(rover.position)
         end
 
         field.forbid(rover.position)
-        rovers.push(rover)
-      end
 
-      rovers
+        rover
+      end
     end
 
     private
 
     attr_accessor :mission_reader
+
+    def build_field(field_data)
+      field_max_x, field_max_y = field_data
+      MarsOne::Models::Field.new(field_max_x, field_max_y)
+    end
+
+    def build_rover(x, y, dir)
+      direction = MarsOne::MissionParser::Utils.direction_to_point(dir)
+      position = MarsOne::Models::Point.new(x, y)
+      MarsOne::Models::Rover.new(position, direction)
+    end
+
+    def apply_command(rover, command_char)
+      command = MarsOne::MissionParser::CommandFactory.command(command_char)
+      command.execute(rover)
+    end
   end
 
 end
